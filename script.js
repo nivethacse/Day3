9313c651d4ab65b6e16656bac60e8457d0d3a3f7
nivethacse <akshaya2d@gmail.com>
2021-08-30 16:16:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+var resume = {
+  name: "Nivetha S",
+  address: "Chennai",
+  phone: 9789996217,
+  Email: "akshaya2d@gmail.com",
+  Dob	: "11-06-1995",
+  FathersName	:"sathiya",
+  MothersName	: "yasodha",
+  LanguageKnown: "english,tamil",
+  Programminglanguage		: "C, C++, JAVA",
+  Databaseandpackages		: "MS office, SQL",
+  Frontend		: "HTML",
+  OperatingSystem: "Windows",
+
+};
+
+for (var data in resume) {
+  console.log(resume[data]);
+}
