2d33f1b1aec16b8f2d99f7a1ac4a7c6ea08909bd
nivethacse <akshaya2d@gmail.com>
2021-08-30 19:26:05 +0530

    Add files via upload

---
+++ 1problem.js
@@ -0,0 +1,32 @@
+var database = {
+    1: {
+      name: "NIVETHA",
+      email: "niveth@gmail.com",
+    },
+    2: {
+      name: "suresh",
+      email: "suresh@gmail.com",
+    },
+  
+    3: {
+      name: "vinoth",
+      email: "vinoth@gmail.com",
+    },
+  
+    4: {
+      name: "kishore",
+      email: "kishore@gmail.com",
+    },
+  };
+  // for each loop
+ Object.keys(database).forEach(function (key, val) {
+  console.log(database);
+});
+
+
+// for in loop
+
+  
+//for (var x in database) {
+ //   console.log(obj[x]);
+ //}
